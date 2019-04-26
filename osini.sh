02e51d54be6d63afead5b5c8b1f2d614e5ab02f0
沧澜 <lirongyi@51ucar.cn>
2019-04-26 14:43:20 +0800

    osini

--- osini.sh
+++ osini.sh
@@ -8,23 +8,23 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 ## 1.2 添加并修改zsh主题
 myZshTheme='local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"\n'
-myZshTheme+="PROMPT='[%n@%m %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)]# '"
+myZshTheme+='PROMPT="[%n@%m %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)]# "'
 myZshTheme+='ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"'
 myZshTheme+='ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "'
 myZshTheme+='ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"'
 myZshTheme+='ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"'
 
-echo -e "$HOME/.oh-my-zsh/themes" > "myrobbyrussell.zsh-theme"
+echo -e "$myZshTheme" > "$HOME/.oh-my-zsh/themes/myrobbyrussell.zsh-theme"
 sed -r 's@ZSH_THEME="robbyrussell"@#ZSH_THEME="robbyrussell"\nZSH_THEME="myrobbyrussell"@g' "$HOME/.zshrc" > "$HOME/.zshrc"
 
 # 1.3 注释.bash_pofile加载逻辑(简单处理即可，因为初始化文件比较简单)
-sed -r 's@(#?)(.*\.bashrc)@#\2@g' $HOME/.bash_profile | sed -r 's@^(#?)(fi)@#\2@g' > "$HOME/.bash_pofile"
+sed -r 's@(#?)(.*\.bashrc)@#\2@g' $HOME/.bash_profile | sed -r 's@^(#?)(fi)@#\2@g' > "$HOME/.bash_profile"
 
 ## 1.4 配置zsh加载.bash_profile文件
 echo "source .bash_profile" >> "$HOME/.zshrc"
 
 # 2. 配置ssh
-if [ ! -d "$HOME/.ssh"];then
+if [ ! -d "$HOME/.ssh" ];then
 	mkdir "$HOME/.ssh"
 fi
 chmod 700 "$HOME/.ssh"
